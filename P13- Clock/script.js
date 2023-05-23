c4b66a6d248afa0cf0a7faf68b6f971678be1804
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-05-23 07:33:04 +0530

    added minutes effect using date object

--- P13- Clock/script.js
+++ P13- Clock/script.js
@@ -1,4 +1,5 @@
-const secondHand = document.querySelector(".second-hand")
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
 function setDate(){
     const now = new Date();
 
@@ -7,6 +8,11 @@ function setDate(){
     // console.log(seconds);
     const secondDegrees = ((seconds/60)*360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+    // used date object to get current minutes
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins/60)*360) +90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
