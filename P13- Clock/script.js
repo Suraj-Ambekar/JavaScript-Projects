43642bbb9126b9f8fd5b69fba1df43f022e116af
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-05-23 09:11:12 +0530

    added hours wise rotation to clock

--- P13- Clock/script.js
+++ P13- Clock/script.js
@@ -14,6 +14,11 @@ function setDate(){
     const mins = now.getMinutes();
     const minsDegrees = ((mins/60)*360) + ((seconds/60)*6) +90;
     minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    // used date object to get current hours
+    const hours = now.getHours();
+    const hoursDegrees = ((hours/12)*360)+  ((mins/60)*30) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
